3354732e52da81b1c2749263c4869bd42f4b1b2f
julia.hx <julia.hexing@gmail.com>
2025-06-08 21:57:43 +0200

    init stage with test cube

--- snakes/src/stage.rs
+++ snakes/src/stage.rs
@@ -1,10 +1,49 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, text::cosmic_text::ttf_parser::ankr::Point};
 
 pub struct StagePlugin;
 
 impl Plugin for StagePlugin {
 	fn build(&self, app: &mut App) {
-		// app.add_systems(Startup, ());
+		app.add_systems(Startup, init_stage);
 		// app.add_systems(Update, ())
 	}
+}
+
+#[derive(Bundle)]
+struct StageBundle {
+	stage_manager: StageManager,
+	camera: Camera3d,
+	transform: Transform,
+	spotlight: PointLight,
+}
+
+#[derive(Component)]
+struct StageManager {
+	current_stage_id: u32,
+}
+
+impl StageManager {
+	fn new() -> Self {
+		Self { current_stage_id: 0 }
+	}
+}
+
+fn init_stage(
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<StandardMaterial>>
+) {
+	commands.spawn(StageBundle{
+		stage_manager: StageManager::new(),
+		camera: Camera3d::default(),
+		transform: Transform::from_xyz(0.0, 10.0, 0.0).looking_at(Vec3::ZERO, Vec3::Z),
+		spotlight: PointLight { shadows_enabled: true, ..default() },
+	});
+
+	// test cube
+	commands.spawn((
+		Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+		MeshMaterial3d(materials.add(Color::srgb_u8(220, 220, 220))),
+		Transform::from_xyz(0.0, 0.0, 0.0),
+	));
 }
--- snakes/src/state.rs
+++ snakes/src/state.rs
@@ -4,12 +4,13 @@ pub struct StatePlugin;
 
 impl Plugin for StatePlugin {
 	fn build(&self, app: &mut App) {
-		app.add_systems(Startup, (init_gamestate));
-		// app.add_systems(Update, ())
+		app.add_systems(Startup, init_gamestate);
+		app.add_systems(Update, update_gamestate);
 	}
 }
 
 enum GameState {
+	Init,
 	Setup,
 	Start,
 	Play,
@@ -27,34 +28,66 @@ struct GameStateData {
 	current_state: GameState,
 }
 
+impl GameStateData {
+	fn new() -> Self {
+		Self { current_state: GameState::Init }
+	}
+
+	fn set_gamestate(&mut self, target_state: GameState) {
+		match target_state {
+			GameState::Init => {
+				println!("game state: Init");
+			}
+			GameState::Setup => {
+				println!("game state: Setup");
+			},
+			GameState::Start => {
+				println!("game state: Start");
+			},
+			GameState::Play => {
+				println!("game state: Play");
+			},
+			GameState::Win => {
+				println!("game state: Win");
+			},
+			GameState::Death => {
+				println!("game state: Death");
+			},
+		}
+
+		self.current_state = target_state;
+	}
+}
+
 fn init_gamestate(mut commands: Commands) {
 	println!("starting snakes game!");
 	
 	commands.spawn(GameStateBundle{
-		data: GameStateData {
-			current_state: GameState::Setup,
-		},
+		data: GameStateData::new(),
 	});
-
-	set_gamestate(GameState::Setup); // TODO move this to game state entity struct
 }
 
-fn set_gamestate(target_state: GameState) {
-	match target_state {
-		GameState::Setup => {
-			println!("game state: Setup");
-		},
-		GameState::Start => {
-			println!("game state: Start");
-		},
-		GameState::Play => {
-			println!("game state: Play");
-		},
-		GameState::Win => {
-			println!("game state: Win");
-		},
-		GameState::Death => {
-			println!("game state: Death");
-		},
+fn update_gamestate(mut query: Query<&mut GameStateData>, time: Res<Time>) {
+	for (mut gs) in &mut query {
+		match gs.current_state {
+			GameState::Init => {
+				gs.set_gamestate(GameState::Setup);
+			}
+			GameState::Setup => {
+				
+			},
+			GameState::Start => {
+				
+			},
+			GameState::Play => {
+				
+			},
+			GameState::Win => {
+				
+			},
+			GameState::Death => {
+				
+			},
+		}
 	}
 }
