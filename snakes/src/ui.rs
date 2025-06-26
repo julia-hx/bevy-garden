df8b9b60e9a3f29950b2f1a528d76f598720fb6c
julia.hx <julia.hexing@gmail.com>
2025-06-26 21:03:07 +0200

    text element with id

--- snakes/src/ui.rs
+++ snakes/src/ui.rs
@@ -4,12 +4,35 @@ pub struct UIPlugin;
 
 impl Plugin for UIPlugin {
 	fn build(&self, app: &mut App) {
-		app.add_systems(Startup, test_text);
+		app.add_systems(Startup, init_ui_fields);
 	}
 }
 
-fn test_text(mut commands: Commands){
+#[derive(Component, Copy, Clone)]
+#[require(Node)]
+struct TextElement {
+	id: &'static str,
+
+}
+
+impl TextElement {
+	fn new(id: &'static str) -> Self {
+		Self {
+			id,
+		}
+	}
+}
+
+fn init_ui_fields(mut commands: Commands) {
 	commands.spawn((
+		TextElement::new("test"),
 		Text::new("hello bevy!"),
+		Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(10.0),
+            left: Val::Px(10.0),
+            ..default()
+        },
+
 	));
 }
